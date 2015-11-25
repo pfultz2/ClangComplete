37f001ce62c4c33d299b6381c67b8c7babfd7ae2
eyenseo <eyenseo@gmail.com>
2015-11-25 18:33:07 +0100

    Add obtaining includes from CMake
    THis was tested with CMake 3.4.0

--- clangcomplete.py
+++ clangcomplete.py
@@ -40,7 +40,7 @@ def debug_print(*args):
 def parse_flags(f):
     flags = []
     for line in open(f).readlines():
-        if line.startswith('CXX_FLAGS') or line.startswith('CXX_DEFINES'):
+        if line.startswith('CXX_FLAGS') or line.startswith('CXX_DEFINES') or line.startswith('CXX_INCLUDES'):
             words = line[line.index('=')+1:].split()
             flags.extend([word for word in words if not word.startswith('-g')])
     return flags
